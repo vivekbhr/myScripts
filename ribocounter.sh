873c1f82a00b101ad2841079e6431ee9f3933d36
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2015-12-17 17:50:11 +0100

    modified

--- ribocounter.sh
+++ ribocounter.sh
@@ -11,7 +11,10 @@ echo -e "Total.alignments" >> total_alignments.txt
 echo -e "Primary.alignments" >> primary_alignments.txt
 echo -e "Primary.rDNA.alignments" >> Primary_rDNA_alignments.txt
 echo -e "Total.reads" >> Total_reads.txt
+echo -e "Mapped.reads" >> Mapped_reads.txt
+echo -e "Primary.Mapped.reads" >> Primary-Mapped_reads.txt
 echo -e "rDNA.reads" >> rDNA_reads.txt
+echo -e "rDNA.reads.primary" >> rDNA_reads_primary.txt
 
 ## count and output
 for file in $(ls | grep .bam$)
@@ -21,18 +24,20 @@ samtools view -c ${file} >> total_alignments.txt
 samtools view -cF260 ${file} >> primary_alignments.txt
 samtools view -cF260 ${file} rDNA >> Primary_rDNA_alignments.txt
 ## Now count reads
-samtools bam2fq ${file} | grep "/1" | wc -l >> Total_reads.txt
-samtools view -bF4 ${file} | samtools bam2fq - | grep "/1" | wc -l >> Mapped_reads.txt
-samtools view -bF260 ${file} | samtools bam2fq - | grep "/1" | wc -l >> Primary-Mapped_reads.txt
+samtools bam2fq ${file} | grep "@M" | wc -l >> Total_reads.txt
+samtools view -bF4 ${file} | samtools bam2fq - | grep "@M" | wc -l >> Mapped_reads.txt
+samtools view -bF260 ${file} | samtools bam2fq - | grep "@M" | wc -l >> Primary-Mapped_reads.txt
 
-samtools view ${file} | grep "NH:i:[0-9]" | samtools bam2fq - | grep "/1" | wc -l >> Multi-Mapped_reads.txt
-samtools view -bF260 ${file} | samtools bam2fq - | grep "/1" | wc -l >> Primary_Multi-Mapped_reads.txt
-samtools view -bF260 ${file} rDNA | samtools bam2fq - | grep "/1" | wc -l >> rDNA_reads.txt
+#samtools view ${file} | grep "NH:i:[0-9]" | samtools bam2fq - | grep "/1" | wc -l >> Multi-Mapped_reads.txt
+#samtools view -bF260 ${file} | samtools bam2fq - | grep "/1" | wc -l >> Primary_Multi-Mapped_reads.txt
+samtools view -bF4 ${file} rDNA | samtools bam2fq - | grep "@M" | wc -l >> rDNA_reads.txt
+samtools view -bF260 ${file} rDNA | samtools bam2fq - | grep "@M" | wc -l >> rDNA_reads_primary.txt
 done
 
 # paste results and remove other files
-paste Filename.txt total_alignments.txt primary_alignments.txt Primary_rDNA_alignments.txt Total_reads.txt rDNA_reads.txt > rDNA_counts_excludeMulti.txt
-rm Filename.txt total_alignments.txt primary_alignments.txt Primary_rDNA_alignments.txt Total_reads.txt rDNA_reads.txt
+paste Filename.txt total_alignments.txt primary_alignments.txt Primary_rDNA_alignments.txt Total_reads.txt Mapped_reads.txt Primary-Mapped_reads.txt rDNA_reads.txt rDNA_reads_primary.txt > rDNA_counts_excludeMulti.txt
+
+rm Filename.txt total_alignments.txt primary_alignments.txt Primary_rDNA_alignments.txt Total_reads.txt rDNA_reads.txt Mapped_reads.txt Primary-Mapped_reads.txt rDNA_reads_primary.txt
 
 ## Run the Rscript
 
