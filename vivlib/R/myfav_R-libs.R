d7024d527eea5798bd8360b794b73c4d6b47a0e6
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-01-04 17:33:22 +0100

    multi package load working

--- vivlib/R/myfav_R-libs.R
+++ vivlib/R/myfav_R-libs.R
@@ -3,22 +3,23 @@
 ## Load a bunch of libraries that I mostly use (NOT WORKING STILL)
 
 ## default pkg options
-usual <- c("plyr","dplyr","reshape2","ggplot2")
-genomic <- c("GenomicRanges","DESeq2","DEXSeq","devtools")
-plotlibs <- c("UpSetR","RColorBrewer","pheatmaps","gridExtra")
-efficient <- c("parallel","doParallel","readr","data.table")
+pkglist <- list(
+        usual = c("plyr","dplyr","reshape2","ggplot2"),
+        genomic = c("GenomicRanges","DESeq2","DEXSeq","devtools"),
+        plotlibs = c("UpSetR","RColorBrewer","pheatmaps","gridExtra"),
+        efficient = c("parallel","doParallel","readr","data.table")
+)
 
+## load pkg list
+loadMyLibs <- function(groupName = "usual",silent=TRUE,pkglist = pkglist){
+        if(groupName %in% names(pkglist)){
+                pkgs = pkglist[[groupName]]
+                ## Load packages
+                lapply(pkgs,require,character.only=TRUE)
+        } else {
+                print("Package list is not predefined")
+        }
 
-loadMyLibs <- function(groupName = "usual",silent=TRUE){
-  
-  ## Load packages
-  pkgs <- groupName
-  if(silent){
-    for(pkg in pkgs) suppressPackageStartupMessages(require(pkg))
-  } else {
-    for(pkg in pkgs) require(pkg)
-  }
-  
 }
 
 
