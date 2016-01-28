d120f6a4c40d71876ba6768cd3824cbf9380257c
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-01-28 16:40:59 +0100

    writes more messages

--- vivlib/R/annotate_DESeq.R
+++ vivlib/R/annotate_DESeq.R
@@ -29,13 +29,16 @@ annotate_DESeqOutput <- function(DESeqOutput, Output, remote = TRUE, genome = "h
                 ensembl = biomaRt::useMart("ensembl",tofetch)
                 ext.data <- biomaRt::getBM(attributes = c("ensembl_gene_id","external_gene_name","description"),
                                 mart = ensembl)
+                message("merging and writing")
                 outfile <- merge(seqout,ext.data,by.x = 0,by.y = 1)
                 write.table(outfile,Output,sep="\t", row.names = FALSE,quote=FALSE)
 
         } else {
                 assertthat::assert_that(assertthat::is.readable(map_file))
+                message("merging and writing")
                 bed <- read.delim(map_file,header = TRUE)
                 outfile <- merge(seqout,bed,by.x = 0,by.y = 1)
                 write.table(outfile,Output,sep="\t", row.names = FALSE,quote=FALSE)
         }
+        message("Done!")
 }
