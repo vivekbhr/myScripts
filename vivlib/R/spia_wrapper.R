a4acf1853e48f079bbc42a03c90b0adbfc115051
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2016-02-10 16:16:38 +0100

    added function to plot a bubble plot for spia_wrapper output

--- vivlib/R/spia_wrapper.R
+++ vivlib/R/spia_wrapper.R
@@ -44,3 +44,35 @@ spia_wrapper <- function(DESeqOut, organism = "mmu", padjCutoff, outFile, outPlo
         dev.off()
         write.table(spia.degenes,outFile, sep = "\t", quote = FALSE, row.names = FALSE)
 }
+
+
+#' Make a bubblePlot for SPIA pathway output
+#'
+#' @param SPIAout output from spia_wrapper
+#' @param outfileName output pdf file name for plot
+#' @param top How many top pathways to plot (by pGFdr value)
+#' @param title Title of the plot
+#'
+#' @return plot A bubbleplot in pdf format
+#' @export
+#'
+#' @examples
+#' spia_plotBubble(spia_wrapper_output, outfileName = "test.out, top = 20, title = "test plot)
+#'
+
+spia_plotBubble <- function(SPIAout,outfileName, top = 20, title = NULL){
+        path <- read.delim(pipe(paste0("cut -f1,3,9,11 ",SPIAout)), header = TRUE)
+        path$pGFdr <- -log10(path$pGFdr)
+        path <- path[order(path$pGFdr,decreasing = TRUE),]
+        path <- path[1:top,]
+
+        pdf(outfileName)
+        print(ggplot2::ggplot(path,aes(Name,pGFdr,fill = Status, size = pSize)) +
+                      ggplot2::geom_point(alpha = 0.7, shape = 21) +
+                      ggplot2::scale_size_area(max_size = 15) + theme_bw(base_size = 15) +
+                      ggplot2::theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+                      ggplot2::scale_fill_manual(values = c("forestgreen","Red")) +
+                      ggplot2::labs(x = "Pathway size", y = "-log10(p-value)",fill = "Status", title = title)
+        )
+        dev.off()
+}
