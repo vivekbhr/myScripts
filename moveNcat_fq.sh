717f0727c84fdff6d835a696716ff920d43b1bbc
vivekbhr <v.bhardwaj@hubrecht.eu>
2020-08-06 16:19:31 +0200

    mod

--- moveNcat_fq.sh
+++ moveNcat_fq.sh
@@ -7,4 +7,4 @@ for R in R1 R2; do cat $dir/*_${R}_*.fastq.gz > $(basename $dir)_${R}.fastq.gz;
 	echo "$(basename $dir)_${R}.fastq.gz $(ls $dir | grep ${R} | grep L001)" >> rename.sh
 done; done
 
-cat rename.sh | sed s/_[A-Z0-9_]*_S[0-9]*_L[0-9]*// | sed s/CG-// | sed s/_001// | xargs -L 1 mv
+cat rename.sh | sed s/_[A-Z0-9_]*_S[0-9]*_L[0-9]*// | sed s/_001// | xargs -L 1 mv
