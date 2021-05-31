4c8918ca33da9c7cf73af5b468cb464cd0618fa0
vivekbhr <v.bhardwaj@hubrecht.eu>
2021-05-31 21:24:23 +0200

    download stuff from ENA, given the tab file with info

---
+++ ENA_download.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+input=$1
+njobs=$2
+num=$(head -1 $input | tr '\t' '\n' | grep -n "fastq_ftp" | cut -d: -f1)
+name=$(head -1 $input | tr '\t' '\n' | grep -n "sample_title" | cut -d: -f1)
+sed 1d $input | cut -f$num | cut -d ';' -f1 > fname1
+sed 1d $input | cut -f$num | cut -d ';' -f2 > fname2
+sed 1d $input | cut -f$name > fname3
+## Fetch read1
+echo "Fetching Read 1 and unpaired reads with suffix: _R1.fastq.gz"
+paste fname1 fname3 | awk 'OFS="\t"{print $2"_R1.fastq.gz", $1}' | xargs -P $njobs -L 1 -n 2 curl -o
+## fetch read2
+echo "Fetching Read 2 for paired reads with suffix: _R2.fastq.gz"
+paste fname2 fname3 | awk '/_2.fastq.gz/ {print $2"_R2.fastq.gz", "\t", $1}' | xargs -P $njobs -L 1 -n 2 curl -o
